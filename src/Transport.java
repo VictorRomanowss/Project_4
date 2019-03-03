294b9f67234854c083a3c44e54521f743f24836e
Виктор Романов <romanow.vitya2018@icloud.com>
2019-03-03 13:07:07 +0300

    init project

---
+++ src/Transport.java
@@ -0,0 +1,91 @@
+import java.sql.SQLOutput;
+
+interface Transport {
+    void move();
+
+    public class ground {
+    }
+
+    public class Auto {
+        private Door leftdoor = new Door();
+
+        private class Door {
+            private String color;
+
+            private Door() {
+                this.color = color;
+            }
+        }
+    }
+
+
+    public class moto {
+    }
+
+    public class velo {
+    }
+
+
+    public abstract class underground implements Transport {
+        @Override
+        public void move(
+        ) {
+        }
+    }
+
+    public abstract class metro implements Transport {
+        @Override
+        public void move() {
+        }
+    }
+
+    public abstract class Relsoviy implements Transport {
+        @Override
+        public void move() {
+        }
+    }
+
+    public class Tramvai {
+        public void tramvai35() {
+            System.out.println("зеленый трамвайка чух-чух");
+        }
+
+    }
+
+    public class Aircraft {
+        public void tu154() {
+            System.out.println("реактивный самолет");
+        }
+    }
+
+    public class helicopters {
+    }
+
+    public class boats {
+
+    }
+
+    public class ships {
+    }
+
+    public class dinghy {
+    }
+
+    public class rockets {
+        public static void main(String[] agrs) {
+            System.out.println("12 апреля 1961 года Юрий гагарин совршил первый полет в космос на корабле Восток-1");
+        }
+
+    }
+
+    public class satellites {
+    }
+
+}
+
+
+
+
+
+
+
